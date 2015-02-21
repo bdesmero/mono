a4ddaa3bf9b7c9f920628342a2e5214de9573dc8
bdesmero <bdesmero@gmail.com>
2015-02-21 10:45:53 +0800

    Initial commit

---
+++ source/javascripts/mono.js
@@ -0,0 +1,38 @@
+//= require_tree .
+
+$(document).ready(function(){
+
+	// Scroll to show/hide navbar
+	var didScroll = false,
+			$window = $(window),
+			$nav = $('nav');
+
+	var aboveThreshold = function(){
+		var scrollTop = $window.scrollTop(),
+				navHeight = ($window.height() - 150);
+
+		return scrollTop > navHeight;
+	};
+
+	$window.scroll(function(){ didScroll = true; });
+
+	setInterval(function(){
+		if (didScroll){
+			didScroll = false;
+
+			if (aboveThreshold()){ $nav.slideDown(); }
+			else { $nav.slideUp(); }
+		}
+	}, 200);
+
+	// Navbar links on click
+	$('.navbar a').on('click', function(e){
+		e.preventDefault();
+		var el = $($(this).attr('href'));
+
+		$('html, body').animate({
+        scrollTop: el.offset().top
+    }, 1000, 'swing');
+	});
+
+});
